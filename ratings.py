32da74d2574f784c188855f5c0a699a6b0d4ca80
Hackbright Student <noreply@hackbrightacademy.com>
2019-04-30 20:14:11 -0700

    define restaurant and ratings function

--- ratings.py
+++ ratings.py
@@ -2,3 +2,23 @@
 
 
 # put your code here
+import sys
+
+def restaurant_ratings(file):
+
+	file = sys.argv[1]
+	
+
+	for line in open(file):
+		restaurants_and_ratings = line.split(":")
+	
+
+	length = len(restaurants_and_ratings)
+	i = 0
+	restaurant_dict = {}
+	while i < length - 1:
+		restaurant_dict[restaurants_and_ratings[i]] = restaurants_and_ratings[i + 1]
+	return restaurant_dict
+
+print(restaurant_ratings("scores.txt"))
+
